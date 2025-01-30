43a5e1f6b30d537a6eb06055c6a9ad0ddcdeafdb
TreVon Encalade <tencalade27@gmail.com>
2025-01-30 17:58:24 -0600

    Update pivoting.sql
    Signed-off-by: TreVon Encalade <tencalade27@gmail.com>

--- dbt Certified Developer/pivoting.sql
+++ dbt Certified Developer/pivoting.sql
@@ -1,23 +1,24 @@
-{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
-
+{%- set payment_methods = ['bank_transfer','credit_card','coupon','gift_card'] -%}
+ 
 with payments as (
-  select * from {{ ref('stg_payments') }}
+   select * from {{ ref('stg_payments') }}
 ),
-
-pivoted as (
-  select
-    order_id,
-    {% for payment_method in payment_methods -%}
-      sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amt,
-    {%- if not loop.last -%}
-      ,
-    {%- endif %}
-    
-    {% endfor -%}
-  
-  from payments
-  where status = 'success'
-  group by order_id
+ 
+final as (
+   select
+       order_id,
+       {% for payment_method in payment_methods -%}
+ 
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) 
+            as {{ payment_method }}_amount
+          
+       {%- if not loop.last -%}
+         ,
+       {% endif -%}
+ 
+       {%- endfor %}
+   from <code class="language-sql">payments</code>
+   group by 1
 )
-
-select * from pivoted;
+ 
+select * from final
