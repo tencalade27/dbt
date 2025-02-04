186b4741bf46f7db3d9842147dff934ba2d40721
TreVon Encalade <tencalade27@gmail.com>
2025-02-04 16:57:02 -0600

    Create union_tables_by_prefix_maco.sql
    Signed-off-by: TreVon Encalade <tencalade27@gmail.com>

---
+++ dbt Certified Developer/macros/union_tables_by_prefix_maco.sql
@@ -0,0 +1,19 @@
+{%- macro union_tables_by_prefix(database, schema, prefix) -%}
+  {%- set tables = dbt_utils.get_relations_by_prefix(database=database, schema=schema, prefix=prefix) -%}
+  {% for table in tables %}
+      {%- if not loop.first -%}
+      union all 
+      {%- endif %}
+      select * from {{ table.database }}.{{ table.schema }}.{{ table.name }}
+  {% endfor -%}
+{%- endmacro -%}
+
+{{ union_tables_by_prefix(
+
+      database='raw',
+      schema='dbt_learn_jinja', 
+      prefix='orders__'
+        
+      )
+      
+  }}
