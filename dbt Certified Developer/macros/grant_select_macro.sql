c532338560ae16e683a4a75a83c5c053541c55f7
TreVon Encalade <tencalade27@gmail.com>
2025-02-04 18:14:00 -0600

    Update grant_select_macro.sql
    Signed-off-by: TreVon Encalade <tencalade27@gmail.com>

--- dbt Certified Developer/macros/grant_select_macro.sql
+++ dbt Certified Developer/macros/grant_select_macro.sql
@@ -10,14 +10,14 @@
 
 {% macro grant_select(schema = target.schema, role = target.role) %}
 
-  {% set name %}
+  {% set sql %}
     grant usage on schema {{ schema }} to role {{ role }};
     grant select on all tables in schema {{ schema }} to role {{ role }};
     grant select on all views in schema {{ schema }} to role {{ role }};
   {% endset %}
   
   {{ log('Granting select on all tacbles and views in schema ' ~ schema ~ 'to role ' ~ role, info = True )}}
-  {% do run_query() %}
+  {% do run_query(sql) %}
   {{ log('Priviliges granted.', info = True) }}
 
 {% endmacro %}
