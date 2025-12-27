a7cf97571319d3c8c642ab533e47939d0e0ca972
TreVon Encalade <tencalade27@gmail.com>
2025-12-27 14:56:14 -0600

    Remove unnecessary whitespace and clean up SQL
    Signed-off-by: TreVon Encalade <tencalade27@gmail.com>

--- dbt_Fundamentals/IDE models/Intermediate Tables/fct_orders.sql
+++ dbt_Fundamentals/IDE models/Intermediate Tables/fct_orders.sql
@@ -1,11 +1,9 @@
 with orders as  (
     select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
-
 payments as (
     select * from {{ ref ('stg_stripe__payments') }}
 ),
-
 order_payments as (
     select
         order_id,
@@ -14,17 +12,13 @@ order_payments as (
     from payments
     group by 1
 ),
-
  final as (
-
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
         coalesce (order_payments.amount, 0) as amount
-
     from orders
     left join order_payments using (order_id)
 )
-
 select * from final
