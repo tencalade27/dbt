dfb5a091c6a5305986e2016263a546cf5c15b3d6
TreVon Encalade <tencalade27@gmail.com>
2026-01-06 17:39:10 -0600

    Add tenure_years and update employee_status logic
    Signed-off-by: TreVon Encalade <tencalade27@gmail.com>

--- HR Analytics Project/models/staging/hr/stg_hr_employees.sql
+++ HR Analytics Project/models/staging/hr/stg_hr_employees.sql
@@ -5,10 +5,14 @@ select
   date_of_birth,
   hire_date,
   termination_date,
-  department_id,
-  manager_id,
+  case 
+    when termination_date is null then date_part('year', current_date) - date_part('year', hire_date)
+    else date_part('year', termination_date) - date_part('year', hire_date)
+  end as tenure_years,
   case
-    when termination_date is null then 'Active'
-    else 'Terminated'
-  end as employee_status
+    when termination_date is null then 'active'
+    else 'terminated'
+  end as employee_status,
+  department_id,
+  manager_id
 from {{ source('hr', 'employees') }}
